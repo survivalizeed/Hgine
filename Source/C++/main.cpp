c1b037d44628e9815ff137f8bf6d0009bc40a08d
survivalizeed <78881613+survivalizeed@users.noreply.github.com>
2021-03-24 01:04:20 +0100

    Add files via upload

--- Source/C++/main.cpp
+++ Source/C++/main.cpp
@@ -17,62 +17,10 @@ int main() {
 	renderer.DebugConsole(_debug);
 	renderer.FPS();
 
-	sur::Instancer::Add(new sur::Object(lua::GetTableContent(L, "configuration", "ressource_path") + "Space.Hgineres",
-		{ 0,0 }, "Space", 100), Types::Obj);
-
-	sur::Instancer::Add(new sur::Object(lua::GetTableContent(L, "configuration", "ressource_path") + "Player.Hgineres",
-		{ 10,400 }, "Player", 1), Types::Obj);
-
-	sur::Instancer::Add(new sur::Object(lua::GetTableContent(L, "configuration", "ressource_path") + "Enemy.Hgineres",
-		{ 0,10 }, "Enemy", 2), Types::Obj);
-
-	sur::Instancer::Add(new sur::Object(lua::GetTableContent(L, "configuration", "ressource_path") + "Shot.Hgineres",
-		{ 0,0 }, "Shot", 3), Types::Obj); // <- template for future shots
-
-	sur::Instancer::Add(new sur::Line({ 0,0 }, { 0,_window_size.y - 1 },
-		Color(0, 0, 0), "LineLeft", 4), Types::Line);
-
-	sur::Instancer::Add(new sur::Line({ _window_size.x - 1,0 }, { _window_size.x - 1,_window_size.y - 1 },
-		Color(0, 0, 0), "LineRight", 4), Types::Line);
-
-	int shotvar = 0;
-	int DelayFromFps = 0;
 	for (;;) {
 		renderer.ClearScreenBuffer();
-		{
-			sur::Instancer::GetObj("Space")->Bind(false, ColliderType::None);
-			sur::Instancer::GetObj("Enemy")->Bind(true, ColliderType::Filled);
-			sur::Instancer::GetObj("Player")->Bind(true, ColliderType::Filled);
-			sur::Instancer::GetLine("LineLeft")->Bind(true);
-			sur::Instancer::GetLine("LineRight")->Bind(true);
-			for (int i = 0; i < shotvar; ++i) {
-				sur::Instancer::GetObj("CloneShot" + std::to_string(i))->Bind(true, ColliderType::Filled);
-				sur::Instancer::GetObj("CloneShot" + std::to_string(i))->Move({ 0,4 }, true);
-			}
-		}
-		{
-			sur::MoveTowards((sur::Master*)(sur::Instancer::GetObj("Enemy")),
-				(sur::Master*)sur::Instancer::GetObj("Player"), { 1,0 }, true);
-		}
-		{
-			if (_input.keyboard.Key(Keys::A))
-				sur::Instancer::GetObj("Player")->Move({ -3,0 }, true);
-			if (_input.keyboard.Key(Keys::D))
-				sur::Instancer::GetObj("Player")->Move({ 3,0 }, true);
-			if (_input.keyboard.Key(Keys::SPACE) && DelayFromFps >= 65) {
-				sur::Instancer::Add(new sur::Object(sur::Instancer::GetObj("Shot"),
-					sur::Instancer::GetObj("Player")->GetPosition(), "CloneShot" + std::to_string(shotvar), 3,
-					[](sur::Master* obj) {
-						if (obj->GetName() == "Enemy")
-							sur::Instancer::State(Types::Obj, false, "Enemy");
-					}
-					), Types::Obj);
-				shotvar++;
-				DelayFromFps = 0;
-			}
-		}
+
 		renderer.RenderScreenBuffer();
-		DelayFromFps ++;
 	}
 }
 
@@ -90,7 +38,7 @@ lua_State* Start() {
 	
 	_Amap(_map, _window_size);
 	
-	//Create the Window with some parameters
+	//Create the window with some parameters
 	Window(lua::GetTableContent(L, "configuration", "application_name").c_str(), WS_OVERLAPPEDWINDOW);
 
 	return L;
