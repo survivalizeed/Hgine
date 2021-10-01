7e60219328af3aa2e9371770cad41bdd8abb5433
survivalizeed <78881613+survivalizeed@users.noreply.github.com>
2021-10-01 23:36:09 +0200

    Update main.cpp

--- New Source/main.cpp
+++ New Source/main.cpp
@@ -1,5 +1,3 @@
-
-
 #include "Hgine/Includes.h"
 
 using namespace sur;
@@ -12,46 +10,55 @@ int main() {
 
 	Renderer renderer(Color(0, 0, 0), true, 0.f, false);
 	renderer.FPS();
-	
-	sur::ParticleSettings settings;
+
+	ParticleSettings3D settings;
 	for (i32 i = 0; i < 255; ++i)
-		settings.colors.push_back(Color(10, i, i / 2));
-	settings.emission = 50000;
-	settings.emission_point_minimal(350, 350);
-	settings.emission_point_maximal(400, 400);
-	settings.min_noise = 1;
-	settings.max_noise = 4;
-	settings.min_distance_to_middle = 50.f;
+		settings.colors.push_back(Color(70, i, i / 2));
+	settings.emission = 100000;
+	settings.emission_point_minimal(100, 100, 100);
+	settings.emission_point_maximal(300, 300, 300);
 	settings.max_distance_to_middle = 200.f;
+	settings.min_distance_to_middle = 100.f;
+	settings.min_noise = 1;
+	settings.max_noise = 20;
+	settings.origin(200, 200, 200);
 
-	sur::ParticleSystem system(&settings);
+	ParticleSystem3D sys(&settings);
 
-	sur::ParticleSettings settings2;
-	for (i32 i = 0; i < 255; ++i)
-		settings2.colors.push_back(Color(10, i32(i / 1.4f), i));
-	settings2.emission = 5000;
-	settings2.emission_point_minimal(350, 350);
-	settings2.emission_point_maximal(400, 400);
-	settings2.min_noise = 1;
-	settings2.max_noise = 5;
-	settings2.max_distance_to_middle = 50.f;
-
-
-	sur::ParticleSystem system2(&settings2);
+	f32 angleX = 0;
+	f32 angleY = 0;
+	f32 angleZ = 0;
 
 	for (;;) {
 		renderer.Clear();
+		sys.Bind(true, 300);
 
-		system.Bind(true);
-		system2.Bind(true);
-		if (Input::Mouse::RClick()) {
-			Vec2f pos = Vec2f(sur::RandomRange(0, 750), sur::RandomRange(0, 750));
-			system.MoveTowards(pos, 0, 4, 6);
-			system2.MoveTowards(pos, 0, 4, 6);
+		if (Input::Keyboard::KeyHeld(J)) {
+			sys.Rotate(Dimension::X, (i32)angleX);
+			angleX += 2.f;
 		}
-		
+		if (Input::Keyboard::KeyHeld(K)) {
+			sys.Rotate(Dimension::Y, (i32)angleY);
+			angleY += 2.f;
+		}
+		if (Input::Keyboard::KeyHeld(L)) {
+			sys.Rotate(Dimension::Z, (i32)angleZ);
+			angleZ += 2.f;
+		}		
+		if (Input::Keyboard::KeyHeld(W))
+			sys.Move({ 0,0,1 });
+		if (Input::Keyboard::KeyHeld(A))
+			sys.Move({ -1,0,0 });
+		if (Input::Keyboard::KeyHeld(S))
+			sys.Move({ 0,0,-1 });
+		if (Input::Keyboard::KeyHeld(D))
+			sys.Move({ 1,0,0 });
+		if (Input::Keyboard::KeyHeld(E))
+			settings.max_distance_to_middle -= 0.5f;
+		if (Input::Keyboard::KeyHeld(Q))
+			settings.max_distance_to_middle += 0.5f;
 
 		renderer.Render();
 	}
-	
+
 }
