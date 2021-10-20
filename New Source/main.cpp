401560461d63b47749831a054ad52f81fc76e84a
survivalizeed <78881613+survivalizeed@users.noreply.github.com>
2021-10-20 15:29:29 +0200

    Add files via upload

--- New Source/main.cpp
+++ New Source/main.cpp
@@ -1,64 +1,81 @@
+
+
 #include "Hgine/Includes.h"
 
 using namespace sur;
 
-
 int main() {
 
-	wndInitialize(Vec2(750, 750), Vec2(750, 750), "", "");
+	hgineInitialize(Vec2(750, 750), Vec2(750, 750), "", "");
 	wndCreate("Hgine Demo", WS_OVERLAPPEDWINDOW);
 
 	Renderer renderer(Color(0, 0, 0), true, 0.f, false);
 	renderer.FPS();
 
-	ParticleSettings3D settings;
-	for (i32 i = 0; i < 255; ++i)
-		settings.colors.push_back(Color(70, i, i / 2));
-	settings.emission = 100000;
-	settings.emission_point_minimal(100, 100, 100);
-	settings.emission_point_maximal(300, 300, 300);
-	settings.max_distance_to_middle = 200.f;
-	settings.min_distance_to_middle = 100.f;
-	settings.min_noise = 1;
-	settings.max_noise = 20;
-	settings.origin(200, 200, 200);
+	Square s1({ 0, 0 }, { 100,100 }, Color(255, 255, 255), Collider::AABB, "s1");
+	Square s2({ 150, 150 }, { 250,250 }, Color(255, 255, 255), Collider::AABB, "s2");
+	Square s3({ 300,300 }, { 400,400 }, Color(255, 255, 255), Collider::AABB, "s3");
 
-	ParticleSystem3D sys(&settings);
+	s1.onCollisionEnter = [](GameObject* current, GameObject*) {
+		oCEnterLockBegin(current);
+		current->color = Color(255, 0, 0);
+		oCEnterLockEnd(current);
+	};
 
-	f32 angleX = 0;
-	f32 angleY = 0;
-	f32 angleZ = 0;
+	s1.onCollisionExit = [](GameObject* current, GameObject*) {
+		oCExitLockBegin(current);
+		if(!current->anyCollisionLeft)
+			current->color = Color(255, 255, 255);
+		oCExitLockEnd(current);
+	};
+
+	s2.onCollisionEnter = [](GameObject* current, GameObject*) {
+		oCEnterLockBegin(current);
+		current->color = Color(255, 0, 0);
+		oCEnterLockEnd(current);
+	};
+
+	s2.onCollisionExit = [](GameObject* current, GameObject*) {
+		oCExitLockBegin(current);
+		current->color = Color(255, 255, 255);
+		oCExitLockEnd(current);
+	};
+
+	s3.onCollisionEnter = [](GameObject* current, GameObject*) {
+		oCEnterLockBegin(current);
+		current->color = Color(255, 0, 0);
+		oCEnterLockEnd(current);
+	};
+
+	s3.onCollisionExit = [](GameObject* current, GameObject*) {
+		oCExitLockBegin(current);
+		current->color = Color(255, 255, 255);
+		oCExitLockEnd(current);
+	};
 
 	for (;;) {
 		renderer.Clear();
-		sys.Bind(true, 300);
+		s1.Bind(true);
+		s2.Bind(true);
+		s3.Bind(true);
 
-		if (Input::Keyboard::KeyHeld(J)) {
-			sys.Rotate(Dimension::X, (i32)angleX);
-			angleX += 2.f;
-		}
-		if (Input::Keyboard::KeyHeld(K)) {
-			sys.Rotate(Dimension::Y, (i32)angleY);
-			angleY += 2.f;
-		}
-		if (Input::Keyboard::KeyHeld(L)) {
-			sys.Rotate(Dimension::Z, (i32)angleZ);
-			angleZ += 2.f;
-		}		
-		if (Input::Keyboard::KeyHeld(W))
-			sys.Move({ 0,0,1 });
 		if (Input::Keyboard::KeyHeld(A))
-			sys.Move({ -1,0,0 });
-		if (Input::Keyboard::KeyHeld(S))
-			sys.Move({ 0,0,-1 });
+			s1.Move({ -0.3f,0 }, 0);
 		if (Input::Keyboard::KeyHeld(D))
-			sys.Move({ 1,0,0 });
-		if (Input::Keyboard::KeyHeld(E))
-			settings.max_distance_to_middle -= 0.5f;
-		if (Input::Keyboard::KeyHeld(Q))
-			settings.max_distance_to_middle += 0.5f;
+			s1.Move({ 0.3f,0 }, 0);
+		if (Input::Keyboard::KeyHeld(W))
+			s1.Move({ 0,0.3f }, 0);
+		if (Input::Keyboard::KeyHeld(S))
+			s1.Move({ 0,-0.3f }, 0);
 
+		if (Input::Keyboard::KeyHeld(J))
+			s2.Move({ -0.3f,0 }, 0);
+		if (Input::Keyboard::KeyHeld(L))
+			s2.Move({ 0.3f,0 }, 0);
+		if (Input::Keyboard::KeyHeld(I))
+			s2.Move({ 0,0.3f }, 0);
+		if (Input::Keyboard::KeyHeld(K))
+			s2.Move({ 0,-0.3f }, 0);
 		renderer.Render();
 	}
-
 }
