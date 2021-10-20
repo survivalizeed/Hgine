1456b782f0f0d5c39c7cba7207ae3d08b76d3882
survivalizeed <78881613+survivalizeed@users.noreply.github.com>
2021-10-20 22:45:25 +0200

    Add files via upload

--- New Source/main.cpp
+++ New Source/main.cpp
@@ -9,55 +9,23 @@ int main() {
 	hgineInitialize(Vec2(750, 750), Vec2(750, 750), "", "");
 	wndCreate("Hgine Demo", WS_OVERLAPPEDWINDOW);
 
-	Renderer renderer(Color(0, 0, 0), true, 0.f, false);
+	Renderer renderer(Color(0, 0, 0), false, 0.1f, true);
 	renderer.FPS();
 
-	Square s1({ 0, 0 }, { 100,100 }, Color(255, 255, 255), Collider::AABB, "s1");
-	Square s2({ 150, 150 }, { 250,250 }, Color(255, 255, 255), Collider::AABB, "s2");
-	Square s3({ 300,300 }, { 400,400 }, Color(255, 255, 255), Collider::AABB, "s3");
+	Square s1({ 0, 0 }, { 750,750 }, Color(255, 255, 255), Collider::AABB, "s1");
+	
+	Light s({ 0,0 }, 0.f, 10000.f, 10.f, Color(255, 255, 0));
 
-	s1.onCollisionEnter = [](GameObject* current, GameObject*) {
-		oCEnterLockBegin(current);
-		current->color = Color(255, 0, 0);
-		oCEnterLockEnd(current);
-	};
-
-	s1.onCollisionExit = [](GameObject* current, GameObject*) {
-		oCExitLockBegin(current);
-		if(!current->anyCollisionLeft)
-			current->color = Color(255, 255, 255);
-		oCExitLockEnd(current);
-	};
-
-	s2.onCollisionEnter = [](GameObject* current, GameObject*) {
-		oCEnterLockBegin(current);
-		current->color = Color(255, 0, 0);
-		oCEnterLockEnd(current);
-	};
-
-	s2.onCollisionExit = [](GameObject* current, GameObject*) {
-		oCExitLockBegin(current);
-		current->color = Color(255, 255, 255);
-		oCExitLockEnd(current);
-	};
-
-	s3.onCollisionEnter = [](GameObject* current, GameObject*) {
-		oCEnterLockBegin(current);
-		current->color = Color(255, 0, 0);
-		oCEnterLockEnd(current);
-	};
-
-	s3.onCollisionExit = [](GameObject* current, GameObject*) {
-		oCExitLockBegin(current);
-		current->color = Color(255, 255, 255);
-		oCExitLockEnd(current);
-	};
+	Light ss({ 0,0 }, 0.f, 10000.f, 10.f, Color(0, 255, 255));
 
 	for (;;) {
 		renderer.Clear();
 		s1.Bind(true);
-		s2.Bind(true);
-		s3.Bind(true);
+
+		if (Input::Mouse::RClick())
+			s.position = Input::Mouse::Position();
+		if (Input::Mouse::LClick())
+			ss.position = Input::Mouse::Position();
 
 		if (Input::Keyboard::KeyHeld(A))
 			s1.Move({ -0.3f,0 }, 0);
@@ -67,15 +35,6 @@ int main() {
 			s1.Move({ 0,0.3f }, 0);
 		if (Input::Keyboard::KeyHeld(S))
 			s1.Move({ 0,-0.3f }, 0);
-
-		if (Input::Keyboard::KeyHeld(J))
-			s2.Move({ -0.3f,0 }, 0);
-		if (Input::Keyboard::KeyHeld(L))
-			s2.Move({ 0.3f,0 }, 0);
-		if (Input::Keyboard::KeyHeld(I))
-			s2.Move({ 0,0.3f }, 0);
-		if (Input::Keyboard::KeyHeld(K))
-			s2.Move({ 0,-0.3f }, 0);
 		renderer.Render();
 	}
 }
